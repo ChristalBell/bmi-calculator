3ed2ddf73496dd43d0d63aa024e48206aa9fc765
Christal Bell <christalvictoriabell@gmail.com>
2025-01-26 23:40:54 -0800

    colors

---
+++ styles/colors.ts
@@ -0,0 +1,11 @@
+export const COLORS = {
+  skyBlue: "#e9f8fe",
+  blue: "#3260f6",
+  pink: " #ffd9ec",
+  orange: "#fbede4",
+  teal: "#dcf2f8",
+  yellow: "#fff6a1",
+  purple: "#ad8fda",
+  white: "#FFFFFF",
+  black: "#2a3348",
+};
